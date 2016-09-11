743c9e2e055dee3f0cb9fb1db5c656ae298d6b1b
DausR <hello@daus.xyz>
2016-09-11 13:15:08 +0800

    Revert "Nuke horrible AI. Close #1842"
    This reverts commit 8499afd68a7a5238c88afec06519cfc80013c850.

--- src/pocketmine/plugin/PluginManager.php
+++ src/pocketmine/plugin/PluginManager.php
@@ -716,6 +716,11 @@ class PluginManager{
 				$this->server->getLogger()->logException($e);
 			}
 		}
+
+		if($this->server->getAIHolder() != null) {
+			if($event instanceof EntityDeathEvent) $this->server->getAIHolder()->MobDeath($event);
+			if($event instanceof EntityDamageByEntityEvent) $this->server->getAIHolder()->EntityDamage($event);
+		}
 	}
 
 	/**
